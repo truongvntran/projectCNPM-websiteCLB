56071819c19c04d3b64c017c3a5dc3fab4141f35
unknown <duledule1914@gmail.com>
2022-05-14 20:47:35 +0700

    demoHomepage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+const path = require('path');
 const express = require('express')
+const { engine } = require('express-handlebars');
 const app = express()
 const port = 3000
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+      extended: true,
+    }),
+  );
+app.use(express.json());
+
+
+app.engine(
+    'hbs',
+    engine({
+      extname: '.hbs',
+    }),
+  );
+  
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
